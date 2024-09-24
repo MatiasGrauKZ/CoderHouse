817135bc4394b903054e042f1ed79b956d0d70a4
matias grau <matugrau@gmail.com>
2024-09-24 19:10:19 -0300

    se agrega la funcionalidad view

---
+++ coderProyecto/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for coderProyecto project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'coderProyecto.settings')
+
+application = get_asgi_application()
