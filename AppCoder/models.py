1fcf926784249287407cac66a2e2da42c9f0eca8
matias grau <matugrau@gmail.com>
2024-09-24 21:21:54 -0300

    creacion models

--- AppCoder/models.py
+++ AppCoder/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Trabajo(models.Model):
+    nombre=models.CharField(max_length=40)
+    codigo=models.IntegerField()
+
+class Trabajador(models.Model):
+    nombre=models.CharField(max_length=40)
+    apellido=models.CharField(max_length=40)
+    email=models.EmailField()
+
+class Empleador(models.Model):
+    nombre=models.CharField(max_length=40)
+    apellido=models.CharField(max_length=40)
+    email=models.EmailField()
+    ProfesionABuscar=models.CharField(max_length=40)
