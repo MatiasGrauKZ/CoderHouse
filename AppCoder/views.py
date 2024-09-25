8141475d78c0c2f908adc79584dcd8c5bd7a2eeb
matias grau <matugrau@gmail.com>
2024-09-24 23:34:58 -0300

    creacion pagina

--- AppCoder/views.py
+++ AppCoder/views.py
@@ -14,21 +14,6 @@ def trabajador(request):
 def empleador(request):
     return render(request, "empleador.html", {} )
 
-def trabajo(request, nombre):
-    nuevo_trabajo= Trabajo(nombre=nombre)
-    nuevo_trabajo.save()
-    return HttpResponse("""<p> Trabajo: {nuevo_trabajo.nombre}' creado con exito!<p>""")
-
-def trabajador(request,nombre,apellido,email):
-    nuevo_trabajador= Trabajo(nombre=nombre, apellido= apellido, email= email)
-    nuevo_trabajador.save()
-    return HttpResponse("""<p> Trabajor: {nuevo_trabajador.nombre}' creado con exito!<p>""")
-    
-
-def empleador(request,nombre,apellido,email):
-    nuevo_empleador= Trabajo(nombre=nombre, apellido= apellido, email= email)
-    nuevo_empleador.save()
-    return HttpResponse("""<p> Trabajor: {nuevo_trabajador.nombre}' creado con exito!<p>""")
 
 
 
