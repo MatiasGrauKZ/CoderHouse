15d815d2434170df24154e68da6ae437e5ad8ddd
matias grau <matugrau@gmail.com>
2024-09-24 22:47:12 -0300

    creacion de template

--- AppCoder/views.py
+++ AppCoder/views.py
@@ -3,16 +3,33 @@ from django.http import HttpResponse
 from .models import Trabajo, Trabajador, Empleador
 
 
+def trabajo(request, nombre):
+    nuevo_trabajo= Trabajo(nombre=nombre)
+    nuevo_trabajo.save()
+    return HttpResponse("""<p> Trabajo: {nuevo_trabajo.nombre}' creado con exito!<p>""")
+
+def trabajador(request,nombre,apellido,email):
+    nuevo_trabajador= Trabajo(nombre=nombre, apellido= apellido, email= email)
+    nuevo_trabajador.save()
+    return HttpResponse("""<p> Trabajor: {nuevo_trabajador.nombre}' creado con exito!<p>""")
+    
+
+def empleador(request,nombre,apellido,email):
+    nuevo_empleador= Trabajo(nombre=nombre, apellido= apellido, email= email)
+    nuevo_empleador.save()
+    return HttpResponse("""<p> Trabajor: {nuevo_trabajador.nombre}' creado con exito!<p>""")
+
+
 
 
 def inicio(request):
-    return HttpResponse('vista inicio')
+    return render(request, "inicio.html", {} )
 
 def trabajo(request):
-    return HttpResponse('vista trabajos')
+    return render(request, "trabajo.html", {} )
 
 def trabajador(request):
-    return HttpResponse('vista trabajador')
+    return render(request, "trabajador.html", {} )
 
 def empleador(request):
-    return HttpResponse('vista empleador')
+    return render(request, "empleador.html", {} )
